64265eb0a3d4eb4522475d8527d5f992bea9f52e
lRyoTs <114388830+lRyoTs@users.noreply.github.com>
2024-06-16 15:51:09 +0200

    Add loader

---
+++ Assets/TF_Project/Scripts/LoadingScene/Loader.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    private static Action loaderCallbackAction;
+
+    //Scene List
+    public enum Scene
+    {
+        MainMenu,
+        LoadingScene,
+        BattleScene,
+        Zone1,
+        Zone2,
+        BossZone
+    }
+
+    private static Scene sceneAux;
+
+    public static void Load(Scene scene)
+    {
+        loaderCallbackAction = () =>
+        {
+            SceneManager.LoadScene(scene.ToString());
+        };
+
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+    }
+
+    public static void Load(string scene)
+    {
+        loaderCallbackAction = () =>
+        {
+            SceneManager.LoadScene(scene);
+        };
+
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+    }
+
+    public static void LoaderCallback()
+    {
+        if (loaderCallbackAction != null)
+        {
+            loaderCallbackAction();
+            loaderCallbackAction = null;
+        }
+    }
+
+    public static string GetCurrentScene()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+}
